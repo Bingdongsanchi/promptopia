6d719681cf824cda0a3c0183495c638d315525e8
Bingdongsanchi <chrisau418@gmail.com>
2023-05-20 23:17:40 -0700

    tailwind

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,12 @@
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center 
+    flex-col">
+        <h1 className="head_text text-center">
+            Discover & Share
+        </h1>
+    </section>
   )
 }
 
