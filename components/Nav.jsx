a4a18b87aba5e08b004f4109988c8a5c728acc4d
Bingdongsanchi <chrisau418@gmail.com>
2023-05-21 22:20:05 -0700

    sign out

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,6 +7,7 @@ import { signIn, signOut, useSession,
 getProviders } from 'next-auth/react';
 
 const Nav = () => {
+    const isUserLoggedIn = true;
   return (
     <nav className="flex-between w-full mb-16
     pt-3">
@@ -19,8 +20,39 @@ const Nav = () => {
             height={30}
             className="object-contain"
             />
+            <p className="logo_text">Promptopia</p>
         </Link>
+        {/* Desktop Navigation */}
+        <div className="sm:flex hidden">
+            {isUserLoggedIn ? (
+                <div className="flex gap-3 md:gap-5">
+                    <Link href="/create-prompt"
+                    className="black_btn">
+                    Create Post 
+                    </Link>
 
+                    <button type="button" onClick=
+                    {signOut} className="outline_btn">
+                        Sign Out
+                    </button>
+                    <Link href="/profile">
+                        <Image
+                            src="/assets prompts/images/logo.svg"
+                            width={37}
+                            height={37}
+                            className="round-full"
+                            alt="profile"
+                        />
+                    </Link>
+
+                </div>
+            ): (
+                <>
+                </>
+            )
+            
+            }
+        </div>
     </nav>
   )
 }
