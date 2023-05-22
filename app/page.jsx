13f81b5ed307f4ff507a4f7158b75a4afdad8458
Bingdongsanchi <chrisau418@gmail.com>
2023-05-21 21:34:25 -0700

    adding a bit

--- app/page.jsx
+++ app/page.jsx
@@ -6,6 +6,9 @@ const Home = () => {
     flex-col">
         <h1 className="head_text text-center">
             Discover & Share
+            <br className="max-md:hidden" />
+            <span className="orange_gradient 
+            text-center">AI-Powered Prompts</span>
         </h1>
     </section>
   )
